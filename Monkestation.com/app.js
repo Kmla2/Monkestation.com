92524ec95ce8c8c7f9c81f2baaea5a78a80bf395
lmenvs <85052537+lmenvs@users.noreply.github.com>
2023-04-23 10:57:55 -0700

    Upload existing files to repo

---
+++ Monkestation.com/app.js
@@ -0,0 +1,55 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+function openRule(evt, ruleIndex) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  unclicked = false;
+  if(unclicked = false)
+    tablinks[0].className = tablinks[1].className.replace("", " active");
+    unclicked = true;
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(ruleIndex).style.display = "block";
+  evt.currentTarget.className += " active";
+
+  
+}
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function mobileNav() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
